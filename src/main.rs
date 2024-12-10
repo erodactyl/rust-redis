1054499d1508dd8d59b3cbf3343458f9f54a4d8e
Erik Davtyan <erikdavtyan@gmail.com>
2024-12-10 13:51:12 +0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use std::{
     io::{prelude::*, BufReader, Write},
     net::{TcpListener, TcpStream},
@@ -9,14 +10,14 @@ enum Command {
     Echo(String),
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
                 thread::spawn(|| {
-                    handle_connection(_stream);
+                    handle_connection(_stream).unwrap();
                 });
             }
             Err(e) => {
@@ -24,10 +25,12 @@ fn main() {
             }
         }
     }
+
+    Ok(())
 }
 
-fn handle_connection(stream: TcpStream) {
-    let mut writer = stream.try_clone().unwrap();
+fn handle_connection(stream: TcpStream) -> Result<()> {
+    let mut writer = stream.try_clone()?;
     let buf_reader = BufReader::new(&stream);
 
     // State
@@ -39,53 +42,59 @@ fn handle_connection(stream: TcpStream) {
 
     loop {
         let line = lines_iterator.next();
-        if let None = line {
-            // Connection closed
+        if let Some(line) = line {
+            let line = line?.trim().to_string();
+
+            if line.starts_with("*") {
+                let message_length = line[1..].parse()?;
+                current_message_length = Some(message_length);
+            } else if line.starts_with("$") {
+                // Bulk string, next line contains a string, read it into current_message
+                let line = lines_iterator.next().unwrap().unwrap().trim().to_string();
+                current_message.push(line);
+                read_lines += 1;
+            }
+
+            if let Some(msg_length) = current_message_length {
+                if msg_length == read_lines {
+                    let command = get_command_from_message(&current_message);
+                    handle_command(command, &mut writer)?;
+
+                    // Reset state
+                    current_message = vec![];
+                    current_message_length = None;
+                    read_lines = 0;
+                }
+            }
+        } else {
             break;
         }
+    }
 
-        let line = line.unwrap().unwrap().trim().to_string();
+    Ok(())
+}
 
-        if line.starts_with("*") {
-            let message_length = line[1..].parse().unwrap();
-            current_message_length = Some(message_length);
-        } else if line.starts_with("$") {
-            // Bulk string, next line contains a string, read it into current_message
-            let line = lines_iterator.next().unwrap().unwrap().trim().to_string();
-            current_message.push(line);
-            read_lines += 1;
-        }
-
-        if let Some(msg_length) = current_message_length {
-            if msg_length == read_lines {
-                handle_message(&current_message, &mut writer);
-
-                // Reset state
-                current_message = vec![];
-                current_message_length = None;
-                read_lines = 0;
-            }
-        }
+fn get_command_from_message(message: &Vec<String>) -> Command {
+    match message[0].to_uppercase().as_str() {
+        "PING" => Command::Ping,
+        "ECHO" => Command::Echo(message[1].clone()),
+        _ => panic!("No pattern found"),
     }
 }
 
-fn handle_message(message: &Vec<String>, writer: &mut TcpStream) {
-    let mut answer = String::new();
-    match message[0].to_uppercase().as_str() {
-        "PING" => {
-            answer = String::from("+PONG\r\n");
+fn handle_command(command: Command, writer: &mut TcpStream) -> Result<()> {
+    match command {
+        Command::Ping => {
+            writer.write_all(b"+PONG\r\n")?;
         }
-        "ECHO" => {
-            // Echo command can have only 1 argument
-            let line = &message[1];
-            answer.push_str("$");
-            answer.push_str(line.len().to_string().as_str());
-            answer.push_str("\r\n");
-            answer.push_str(line.as_str());
-            answer.push_str("\r\n");
+        Command::Echo(line) => {
+            writer.write_all(b"$")?;
+            writer.write_all(line.len().to_string().as_bytes())?;
+            writer.write_all(b"\r\n")?;
+            writer.write_all(line.as_bytes())?;
+            writer.write_all(b"\r\n")?;
         }
-        _ => panic!("No pattern found"),
     }
 
-    writer.write_all(answer.as_bytes());
+    Ok(())
 }
